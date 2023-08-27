63704377e86cea639371880808aa79fb312dae38
OmarLandaAlpha <omar.landa.calderon@outlook.com>
2023-08-27 14:56:30 -0500

    ksjdl

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const secret = secrets.MY_SECRET;
-console.log(`El valor del secreto es: ${secret}`);
+// Obtener el valor pasado como parámetro en la URL
+const urlParams = new URLSearchParams(window.location.search);
+const secretValue = urlParams.get("pinpon_secret");
+
+// Mostrar el valor en la consola
+console.log(`El valor del secreto es: ${secretValue}`);
