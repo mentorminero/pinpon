e042ffcaa72ee6fa4bd1a0c3d247ef2a60731344
OmarLandaAlpha <omar.landa.calderon@outlook.com>
2023-08-27 17:02:41 -0500

    nasdmnasmd

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Accede al atributo "data-secreto" del script para obtener el secreto
-const script = document.querySelector('script[data-secreto]');
-const secreto = script.getAttribute('data-secreto');
+// script.js
 
-console.log(`El valor del secreto es: ${secreto}`);
+// Accede al valor de la variable de entorno MI_SECRETO
+const secreto = window.MI_SECRETO || "Valor predeterminado si el secreto no está disponible";
 
+// Actualiza el contenido del elemento con id "secreto-value" con el valor del secreto
+document.getElementById("secreto-value").textContent = `El valor del secreto es: ${secreto}`;
