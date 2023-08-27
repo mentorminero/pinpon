cafaa17437d3c2ecdd94864cf014e80dd6f1db0e
OmarLandaAlpha <omar.landa.calderon@outlook.com>
2023-08-27 12:22:55 -0500

    tres

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const secretValue = process.env['INPUT_PINPON_SECRET'];
+var secretValue = process.env['INPUT_PINPON_SECRET'];
 console.log(`El valor del secreto "pinpon_secret" es: ${secretValue}`);
