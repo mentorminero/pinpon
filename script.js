01ed772b61710595c050f89f8af23e743e2b7993
OmarLandaAlpha <omar.landa.calderon@outlook.com>
2023-08-27 15:51:40 -0500

    lkao

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Obtener el valor pasado como parámetro en la URL
-const urlParams = new URLSearchParams(window.location.search);
-const secretValue = urlParams.get("pinpon_secret");
+// Obtener el valor del secreto desde una variable de entorno de JavaScript
+const secretValue = process.env.PINPON_SECRET;
 
 // Mostrar el valor en la consola
 console.log(`El valor del secreto es: ${secretValue}`);
